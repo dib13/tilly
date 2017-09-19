7fc12356975700a5d6dfa7ecc8b07336cf2415e5
dib13 <31906180+dib13@users.noreply.github.com>
2017-09-19 14:24:07 -0400

    New exercises done

---
+++ X1p4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+    int x = 0, y = 0;
+    std::cout << "Enter two numbers: " << std::endl;
+    std::cin >> x >> y;
+    std::cout << "The product of the two numbers is: "
+            << (x*y) << std::endl;
+    return 0;
+}
